8d3d9c56994e80b24e4960485eb5fb5f17ba57a3
andrewgray0702 <andrew.gray@midlandu.edu>
2019-04-11 13:05:23 -0500

    added code

--- calculatorAG.js
+++ calculatorAG.js
@@ -1,12 +1,61 @@
 var resWindow = document.querySelector('.resWindow');
 var resString = "";
 var resNum = 0;
-document.querySelectorAll('.numButtons').addEventListener('click',function(){
-    var num = document.querySelectorAll('.numButtons').getAttribute('data-value');
-    resWindow.innerHTML = num;
-})
+var curNum = 0;
+var stringWindow = document.querySelector('.stringWindow');
 
 function clear(){
     resString.clear();
     resNum.clear();
 }
+
+function clearEntry(){
+    curNum = 0;
+}
+
+function getNum(){
+    resWindow.innerHtml = curNum;
+    resString = resString + curNum;
+
+}
+
+function getOp(){
+
+}
+
+function addRes(){
+    resNum = curNum + resNum;
+}
+
+function subRes(){
+    resNum = resNum - curNum;
+}
+
+function mulRes(){
+    resNum = resNum * curNum;
+}
+
+function divRes(){
+    resNum = resNum / curNum;
+}
+
+document.querySelectorAll('.numButtons').addEventListener('click',function(){
+    curNum = this.data-value;
+    stringWindow.append(curNum);
+    document.querySelector(".addButton").addEventListener('click', function(curNum){
+        addRes();
+        resWindow.innerHtml = resNum;
+    })
+    document.querySelector('.subButton').addEventListener('click', function(){
+        subRes();
+        resWindow.innerHtml = resNum;
+    })
+    document.querySelector('.mulButton').addEventListener('click', function(){
+        mulRes();
+        resWindow.innerHtml = resNum;
+    })
+    document.querySelector('.divButton').addEventListener('click', function(){
+        divRes();
+        resWindow.innerHtml = resNum;
+    })
+})
