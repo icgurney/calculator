59752629083db2759f3f634766c80b3e3d16a018
andrewgray0702 <andrew.gray@midlandu.edu>
2019-04-11 11:52:10 -0500

    Update calculatorAG.js

--- calculatorAG.js
+++ calculatorAG.js
@@ -1,4 +1,12 @@
-document.querySelectorAll('.buttons').addEventListener('click',function(){
-    var num = document.querySelector('.resWindow').getAttribute('data-value');
+var resWindow = document.querySelector('.resWindow');
+var resString = "";
+var resNum = 0;
+document.querySelectorAll('.numButtons').addEventListener('click',function(){
+    var num = document.querySelectorAll('.numButtons').getAttribute('data-value');
     resWindow.innerHTML = num;
 })
+
+function clear(){
+    resString.clear();
+    resNum.clear();
+}
