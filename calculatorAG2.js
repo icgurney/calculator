5124c93b4fc4cd5468b8891ac348b579096e72f5
Ian Gurney <ianclaytongurney@gmail.com>
2019-04-12 07:09:25 -0500

    added clear entry

--- calculatorAG2.js
+++ calculatorAG2.js
@@ -11,6 +11,9 @@ document.querySelector('.equalButton').addEventListener('click', function(){
 document.querySelector('.clearButton').addEventListener('click', function(){
     clear();
 })
+document.querySelector('.clearEntry').addEventListener('click', function(){
+  clearEntry();
+})
 function insert(val){
     document.querySelector('.resWindow').value = document.querySelector('.resWindow').value+val;
 }
@@ -19,4 +22,9 @@ function equal(){
 }
 function clear(){
     document.querySelector('.resWindow').value = "";
+}
+function clearEntry(){
+  var str = document.querySelector('.resWindow').value;
+  var res = str.substring(0,str.length-1);
+  document.querySelector('.resWindow').value = res;
 }
