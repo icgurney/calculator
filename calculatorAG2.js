593dd6a6fa2439530be58767ecfc68df7b3d4eaf
andrewgray0702 <andrew.gray@midlandu.edu>
2019-04-11 21:03:45 -0500

    added new js file that is functioning

---
+++ calculatorAG2.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.numButtons').forEach(function(val){
+    val.addEventListener('click',function(){
+        curNum = this.value;
+        insert(curNum);
+        console.log('clicked!')
+    })
+})
+document.querySelector('.equalButton').addEventListener('click', function(){
+    equal();
+})
+function insert(val){
+    document.querySelector('.resWindow').value = document.querySelector('.resWindow').value+val;
+}
+function equal(){
+    document.querySelector('.resWindow').value = eval(document.querySelector('.resWindow').value)
+}
