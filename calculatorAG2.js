72ca910fee8ec65af01c47e96fb835143d4b57b4
andrewgray0702 <andrew.gray@midlandu.edu>
2019-04-11 21:10:53 -0500

    added clear button

--- calculatorAG2.js
+++ calculatorAG2.js
@@ -8,9 +8,15 @@ document.querySelectorAll('.numButtons').forEach(function(val){
 document.querySelector('.equalButton').addEventListener('click', function(){
     equal();
 })
+document.querySelector('.clearButton').addEventListener('click', function(){
+    clear();
+})
 function insert(val){
     document.querySelector('.resWindow').value = document.querySelector('.resWindow').value+val;
 }
 function equal(){
     document.querySelector('.resWindow').value = eval(document.querySelector('.resWindow').value)
+}
+function clear(){
+    document.querySelector('.resWindow').value = "";
 }
